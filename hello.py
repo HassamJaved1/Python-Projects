c684e7c5718124147d50ea495ad4bf8eb7f32445
Hassam Javed <ultrahassam2000@gmail.com>
2024-06-24 09:28:42 +0500

    removed Banana

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
-thislist = ["apple", "banana", "cherry", "banana", "kiwi"]
+thislist = ["apple", "banana", "cherry", "banana"]
 
 for item in thislist:
     if item == 'banana':
         thislist.remove("banana")
+
+print(thislist)
+thislist = ["apple", "banana", "cherry", "banana"]
+
+for items in thislist:
+  if items in 'banana':
+    thislist.remove("banana")
 
 print(thislist)
