1a57403fbe80cd7c89857a160844f1c33bff517a
Hassam Javed <ultrahassam2000@gmail.com>
2024-06-28 09:16:01 +0500

    "Added import statement, modified list iteration and removal, added datetime print statement, and created Student class with instance and method"

--- hello.py
+++ hello.py
@@ -1,14 +1,35 @@
+import datetime as dt
+
+
 thislist = ["apple", "banana", "cherry", "banana"]
 
 for item in thislist:
-    if item == 'banana':
+    if item == "banana":
         thislist.remove("banana")
+
 
 print(thislist)
 thislist = ["apple", "banana", "cherry", "banana"]
 
 for items in thislist:
-  if items in 'banana':
-    thislist.remove("banana")
+    if items in "banana":
+        thislist.remove("banana")
 
 print(thislist)
+
+print(dt.datetime.now())
+
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sayHello():
+        print("Hello, my name is ")
+
+
+Rafay = Student("Rafay", 21)
+Student.sayHello()
+print(Rafay.name)
+print(Rafay.age)
